3870b25a0ff651e07181500d9a06c5be8ab9ac93
jonathan.zapata <84453014+JonathanZapata1@users.noreply.github.com>
2024-10-29 23:52:00 -0500

    Second commit

--- store/urls.py
+++ store/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
 
-urlpatterns =[
-    path('', views.store, name= "store"),
-    path('cart/', views.cart, name='cart'),
-    path('checkout/', views.checkout, name= "checkout")
+urlpatterns = [
+	
+	path('', views.store, name="store"),
+	path('cart/', views.cart, name="cart"),
+	path('checkout/', views.checkout, name="checkout"),
+	path('process_order/', views.processOrder, name="process_order"),
+	path('update_item/', views.updateItem, name="update_item"),
 
 ]
